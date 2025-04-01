981ee9b9a7d70b5290983badf34116bfd8a8ea2d
JoshBarz <josh.barham@hotmail.co.uk>
2025-04-01 20:36:05 +0100

    more lessons

---
+++ 03_loops/hogwarts.py
@@ -0,0 +1,11 @@
+students = ["Hermione", "Harry", "Ron"]
+
+students = {
+    "Herminone": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco":"Slytherin",
+}
+
+for student in students:
+    print(student, students[student], sep=",")
